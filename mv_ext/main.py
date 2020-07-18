ed22128df4ca08c7a37266313369a3039df0bc79
Jeon-JongChan <loki377312@gmail.com>
2020-07-18 13:59:43 +0000

    complete to move movie file - change title for file overlap

--- mv_ext/main.py
+++ mv_ext/main.py
@@ -3,21 +3,34 @@ import os, sys, shutil
 '''=============================== variable ==============================='''
 ext_list = ['avi','mp4','mkv','mpg','wmv','flv','mov']
 first_path = "./"
-dst = "./destination"
+dst_path = "./destination/"
+
 '''=============================== code ==============================='''
 
 ext_stack = []
-f = open("./filename.txt","w")
-#print(f_list)
+#f = open("./filename.txt",'w+')
 
+if not os.path.exists(dst_path):
+    os.makedirs(dst_path)
+    
 for(path, dir, files) in os.walk(first_path):
     for filename in files:
         f_ext = os.path.splitext(filename)[-1]
         for ext in ext_list:
-            print(f_ext,f_ext == '.'+ext,ext)
             if f_ext == '.'+ext or f_ext == '.'+ext.upper():
-                f.write(filename+'\n')
+                file_path = path+'/'+filename
+                size = os.path.getsize(file_path)
+                mb_size = size / (1024.0 * 1024.0) #MB
+                if mb_size < 100:
+                    print(file_path + " is not movie. byte : ",size)
+                    continue
+                if os.path.exists(dst_path+filename): # file 존재시
+                        print(file_path+" is existed, move. destination : ", dst_path,filename+"_1")
+                        shutil.move(file_path, dst_path+filename+"_1")
+                        continue
+                print(file_path+" is move. destination : ", dst_path)
+                shutil.move(file_path, dst_path)
+                #f.write(path+'/'+filename+'\n')
         ext_stack.append(f_ext+'\n')
-        
 
-f.close()
+#f.close()
